5d1daad0a7313b50b285e9e8f79b35d67539b19d
LovishJindal2004 <lovish@mindwebtree.com>
2025-01-29 12:20:54 +0530

    --updates
    --updates

--- src/app/modules/admin/exam/exam-list/exam-list.component.ts
+++ src/app/modules/admin/exam/exam-list/exam-list.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import {MatStepperModule} from '@angular/material/stepper';
 
 @Component({
   selector: 'app-exam-list',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatStepperModule],
   templateUrl: './exam-list.component.html',
   styleUrl: './exam-list.component.scss'
 })
 export class ExamListComponent {
-
+  deleteExam(){}
+  openReportCard(){}
 }
